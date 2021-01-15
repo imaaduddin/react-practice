f1d4270de1ee8cf10ff413507e3faf9c8b0de54b
Imaad Uddin <imaaduddin@icloud.com>
2021-01-14 22:33:10 -0600

    created a component

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // Create a react component 
+const App = () => {
+  return <div>Hi There!</div>;
+};
 
 // Take the react component and show it on the screen
